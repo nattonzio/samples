9ef51c5b403c705e191d69e5a8537519a7a6dab3
nattogaboh <nattogaboh@natto-desktop>
2015-03-06 20:24:51 -0500

    Tercer Commit

---
+++ NetBeansProjects/Ejer4/src/Practica/NattoResponde.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Practica;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author nattogaboh
+ */
+public class NattoResponde {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int tecla,preg=0,cont=0;
+        String p="Natto, favor de responder esta pregunta:";
+        String f="Hola Malditos";
+        String l[]={p,f};
+        JOptionPane.showMessageDialog(null,l[1]);
+        
+    }
+    
+}
